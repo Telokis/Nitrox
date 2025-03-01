766e1a9e61fdf10d40929e0604edbe74e1688155
Telokis <6382729+Telokis@users.noreply.github.com>
2025-03-01 23:35:54 +0100

    Way simplified setup

--- bundle.sh
+++ bundle.sh
@@ -1,25 +1,47 @@
 #!/bin/bash
 
-set -ex
+set -e
 
-OUT_DIR="${1:-./bundle_out}"
-RUNTIME="${2:-win-x64}"
+OUT_DIR="${1:-./bundle}"
 
-echo "Bundling Nitrox Launcher to $OUT_DIR"
+echo "Bundling Nitrox to $OUT_DIR"
 
 echo "Cleaning output dir"
 rm -rf "$OUT_DIR"
 
+# echo "  ______       _  _   __          __ _        "
+# echo " |  ____|     | || |  \\ \\        / /(_)       "
+# echo " | |__  _   _ | || |   \\ \\  /\\  / /  _  _ __  "
+# echo " |  __|| | | || || |    \\ \\/  \\/ /  | || '_ \\ "
+# echo " | |   | |_| || || |     \\  /\\  /   | || | | |"
+# echo " |_|    \\__,_||_||_|      \\/  \\/    |_||_| |_|"
+# echo "                                              "
+# echo "                                              "
+
 dotnet clean
-
 dotnet restore
-
-dotnet publish -r $RUNTIME -c Release --framework net9.0
+dotnet build -c Release Nitrox.Launcher
 
 echo -e "\nCopying files to output dir..."
-cp -r "Nitrox.Launcher/bin/Release/net9.0/$RUNTIME/publish" "$OUT_DIR"
-cp -r "Nitrox.Launcher/bin/Release/net9.0/$RUNTIME/lib" "$OUT_DIR/lib"
+cp -r "Nitrox.Launcher/bin/Release/net9.0" "$OUT_DIR"
 
-cp "NitroxServer-Subnautica/bin/Release/net9.0/$RUNTIME/publish/NitroxServer-Subnautica.deps.json" "$OUT_DIR/"
+# echo "   _____                                  _       _                     "
+# echo "  / ____|                                | |     (_)                    "
+# echo " | (___    ___  _ __ __   __ ___  _ __   | |      _  _ __   _   _ __  __"
+# echo "  \\___ \\  / _ \\| '__|\\ \\ / // _ \\| '__|  | |     | || '_ \\ | | | |\\ \\/ /"
+# echo "  ____) ||  __/| |    \\ V /|  __/| |     | |____ | || | | || |_| | >  < "
+# echo " |_____/  \\___||_|     \\_/  \\___||_|     |______||_||_| |_| \\__,_|/_/\\_\\"
+# echo "                                                                        "
+# echo "                                                                        "
+
+# RUNTIME="linux-x64"
+# echo "Building with Runtime: $RUNTIME"
+# dotnet clean
+# dotnet restore
+# dotnet publish -r $RUNTIME -c Release --framework net9.0
+
+# echo -e "\nCopying files to output dir..."
+# cp -r "NitroxServer-Subnautica/bin/Release/net9.0/$RUNTIME/publish" "$OUT_DIR/nitrox_server_linux"
+# cp -r "NitroxServer-Subnautica/bin/Release/net9.0/$RUNTIME/lib" "$OUT_DIR/nitrox_server_linux/lib"
 
 echo -e "\nEverything is done!"
