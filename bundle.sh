19ab429f3cba4d588801001211baf3fdc5d2a3da
Telokis <6382729+Telokis@users.noreply.github.com>
2025-03-01 23:13:19 +0100

    Easily switch runtime

--- bundle.sh
+++ bundle.sh
@@ -3,6 +3,7 @@
 set -ex
 
 OUT_DIR="${1:-./bundle_out}"
+RUNTIME="${2:-win-x64}"
 
 echo "Bundling Nitrox Launcher to $OUT_DIR"
 
@@ -13,12 +14,12 @@ dotnet clean
 
 dotnet restore
 
-dotnet publish -r win-x64 -c Release --self-contained --framework net9.0
+dotnet publish -r $RUNTIME -c Release --framework net9.0
 
 echo -e "\nCopying files to output dir..."
-cp -r "Nitrox.Launcher/bin/Release/net9.0/win-x64/publish" "$OUT_DIR"
-cp -r "Nitrox.Launcher/bin/Release/net9.0/win-x64/lib" "$OUT_DIR/lib"
+cp -r "Nitrox.Launcher/bin/Release/net9.0/$RUNTIME/publish" "$OUT_DIR"
+cp -r "Nitrox.Launcher/bin/Release/net9.0/$RUNTIME/lib" "$OUT_DIR/lib"
 
-cp "NitroxServer-Subnautica/bin/Release/net9.0/win-x64/publish/NitroxServer-Subnautica.deps.json" "$OUT_DIR/"
+cp "NitroxServer-Subnautica/bin/Release/net9.0/$RUNTIME/publish/NitroxServer-Subnautica.deps.json" "$OUT_DIR/"
 
 echo -e "\nEverything is done!"
